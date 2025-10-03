cf2723b5c801b670ccfc12d26299672ba651efea
chibaf <chibaf@mac.com>
2025-10-03 14:32:28 +0900

    Add files via upload

--- read_ser.py
+++ read_ser.py
@@ -28,25 +28,27 @@ class sers:
       data2s=line02s.split(",")
       data3s=line03s.split(",")
       data4s=line04s.split(",")
-      if data1s[0]=="01": data1=data1s[3:12]
-      if data1s[0]=="02": data2=data1s[3:12]
-      if data1s[0]=="A1": data3=data1s[1:8]
-      if data1s[0]=="A2": data4=data1s[1:8]
-      if data2s[0]=="01": data1=data2s[3:12]
-      if data2s[0]=="02": data2=data2s[3:12]
-      if data2s[0]=="A1": data3=data2s[1:8]
-      if data2s[0]=="A2": data4=data2s[1:8]
-      if data3s[0]=="01": data1=data3s[3:12]
-      if data3s[0]=="02": data2=data3s[3:12]
-      if data3s[0]=="A1": data3=data3s[1:8]
-      if data3s[0]=="A2": data4=data3s[1:8]
-      if data4s[0]=="01": data1=data4s[3:12]
-      if data4s[0]=="02": data2=data4s[3:12]
-      if data4s[0]=="A1": data3=data4s[1:8]
-      if data4s[0]=="A2": data4=data4s[1:8]
+      if data1s[0]=="01": data1=data1s[3:13]
+      if data1s[0]=="02": data2=data1s[3:13]
+      if data1s[0]=="A1": data3=data1s[1:9]
+      if data1s[0]=="A2": data4=data1s[1:9]
+      if data2s[0]=="01": data1=data2s[3:13]
+      if data2s[0]=="02": data2=data2s[3:13]
+      if data2s[0]=="A1": data3=data2s[1:9]
+      if data2s[0]=="A2": data4=data2s[1:9]
+      if data3s[0]=="01": data1=data3s[3:13]
+      if data3s[0]=="02": data2=data3s[3:13]
+      if data3s[0]=="A1": data3=data3s[1:9]
+      if data3s[0]=="A2": data4=data3s[1:9]
+      if data4s[0]=="01": data1=data4s[3:13]
+      if data4s[0]=="02": data2=data4s[3:13]
+      if data4s[0]=="A1": data3=data4s[1:9]
+      if data4s[0]=="A2": data4=data4s[1:9]
 #     
+      print(len(data1))
+      print(len(data3))
       return data1+data2+data3+data4
 
 ser=sers()
 while 1:
-  print(ser.read())
+  print(len(ser.read()))
